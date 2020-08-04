ac2196bb1ea52434f6bcd38ee2df639502d8e624
Ramanjs <raman.s2662@gmail.com>
2020-08-04 18:45:27 +0530

    add progress

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function Book(title, author, pages, pagesRead) {
     this.title = title;
     this.author = author;
-    this.pages = pages + ' pages';
+    this.pages = pages;
     this.pagesRead = pagesRead;
 }
 
 const library = [];
-const formElement = document.querySelector('form');
+const formElement = document.querySelector('#new-book-form');
 const booksContainer = document.querySelector('#books-container');
 const addBookspan = document.querySelector('#open-modal');
-const modal = document.querySelector('#modal');
+const modal = document.querySelector('#new-book-modal');
 const closespan = document.querySelector('.close');
 
 addBookspan.addEventListener('click', openForm);
@@ -24,7 +24,7 @@ function addNewBook(e) {
     let pagesRead = document.querySelector('#pages-read').value;
     let newBook = new Book(name, author, pages, pagesRead);
     library.push(newBook);
-    renderBook(newBook, library.length - 1);
+    render();
     formElement.reset();
     closeForm();
 }
@@ -40,19 +40,35 @@ function renderBook(book, index) {
     let bookNode = document.createElement('div');
     bookNode.classList.add('book');
     bookNode.setAttribute('data-index', index);
+    
     let nameNode = getElement(book, 'p', 'title', 'book-name');
+
     let authorNode = getElement(book, 'p', 'author', 'book-author');
+    
     let pagesNode = getElement(book, 'p', 'pages', 'book-pages');
-    let editSpan = getElement(book, 'span', 'read', 'book-read');
+    pagesNode.innerHTML += ' pages';
+    
+    let editProgress = getElement(book, 'span', '', 'book-status');
+    
     let deleteSpan = getElement(book, 'span', '', 'delete-button');
+    
+    let progress = (Number(book.pagesRead) / Number(book.pages)) * 100;
+    progress = roundOffToOneDecimal(progress);
+    
+    editProgress.innerText = `${progress}% Complete`;
+    editProgress.addEventListener('click', editBook);
+    
+    let gradient = `linear-gradient(90deg, #AEFBAE, #AEFBAE ${progress}%, white ${progress}%, white 100%)`;
+    bookNode.style.background = gradient;
+    
     deleteSpan.innerHTML = '&times;';
     deleteSpan.addEventListener('click', deleteBook);
-    editSpan.addEventListener('click', editBook);
+    
     bookNode.appendChild(deleteSpan);
     bookNode.appendChild(nameNode);
     bookNode.appendChild(authorNode);
     bookNode.appendChild(pagesNode);
-    // bookNode.appendChild(readSpan);
+    bookNode.appendChild(editProgress);
     booksContainer.appendChild(bookNode);
 }
 
@@ -60,16 +76,21 @@ function deleteBook() {
     let bookNode = this.parentNode;
     let index = parseInt(bookNode.dataset.index);
     library.splice(index, 1);
-    bookNode.parentNode.removeChild(bookNode);
+    render();
 }
+
+const editBookForm = document.querySelector('#edit-book-form');
+const editFormModal = document.querySelector('#edit-book-modal');
+let bookIndex = 0;
+
+editBookForm.addEventListener('submit', saveEdit);
 
 function editBook() {
     let bookNode = this.parentNode;
-    let index = parseInt(bookNode.dataset.index);
-    let bookObject = library[index];
-    bookObject.read = bookObject.read === 'read' ? 'not read' : 'read';
-    openForm();
-    render();
+    bookIndex = parseInt(bookNode.dataset.index);
+    let bookObject = library[bookIndex];
+    editFormModal.style.display = 'block';
+    editBookForm[0].max = bookObject.pages;
 }
 
 function deleteAllBooks() {
@@ -103,3 +124,16 @@ const pagesRead = document.querySelector('#pages-read');
 pages.addEventListener('change', () => {
     pagesRead.max = pages.value;
 })
+
+function roundOffToOneDecimal(number) {
+    return Math.round(number * 10) / 10;
+}
+
+function saveEdit(event) {
+    event.preventDefault();
+    let pagesRead = editBookForm[0].value;
+    library[bookIndex].pagesRead = pagesRead;
+    editBookForm.reset();
+    editFormModal.style.display = 'none';
+    render();
+}
